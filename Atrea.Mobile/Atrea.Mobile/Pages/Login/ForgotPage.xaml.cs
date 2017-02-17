074e599287c984805890d8ac6bf7dba326630f24
Vasyl Poklitar <poklitar@glooni.com>
2017-02-17 12:11:33 +0200

    create forgot page

---
+++ Atrea.Mobile/Atrea.Mobile/Pages/Login/ForgotPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Atrea.Mobile.Pages.Login
+{
+    public partial class ForgotPage : ContentPage
+    {
+        public ForgotPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
